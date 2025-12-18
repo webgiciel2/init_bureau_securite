46b13b7a9bbac7d023dcdb3dd34ad7253727bd3a
stefLesage <contact@webgiciel.com>
2025-12-18 15:42:13 +0100

    Initial Symfony bundle structure

---
+++ src/DependencyInjection/InitBureauSecuriteExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Webgiciel2\InitBureauSecurite\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class InitBureauSecuriteExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
---
+++ src/InitBureauSecuriteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Webgiciel2\InitBureauSecurite;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InitBureauSecuriteBundle extends Bundle
+{
+}
