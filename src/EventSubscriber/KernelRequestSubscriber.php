c41286d1441ce23760842f30c4d0defe62e38400
stefLesage <contact@webgiciel.com>
2025-12-19 09:44:39 +0100

    Fix EventSubscriber service injection

--- src/EventSubscriber/KernelRequestSubscriber.php
+++ src/EventSubscriber/KernelRequestSubscriber.php
@@ -5,27 +5,34 @@ namespace Webgiciel2\InitBureauSecurite\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 use Webgiciel2\InitBureauSecurite\Service\SecuriteInitializer;
+
+use Webgiciel2\InitBureauSecurite\Service\InitBureauSecuriteManager;
+
 
 class KernelRequestSubscriber implements EventSubscriberInterface
 {
     public function __construct(
+        private InitBureauSecuriteManager $initManager,
         private SecuriteInitializer $initializer
     ) {}
 
     public static function getSubscribedEvents(): array
     {
         return [
-            RequestEvent::class => 'onKernelRequest',
+            KernelEvents::REQUEST => 'onKernelRequest',
         ];
     }
 
     public function onKernelRequest(RequestEvent $event): void
     {
+
+        // uniquement sur la requête principale
         if (!$event->isMainRequest()) {
             return;
         }
 
-        $this->initializer->initializeIfNeeded();
+        $this->initManager->initialize();
     }
 }
