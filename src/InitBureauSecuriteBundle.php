bc882acb2c32ac403c39c6d42d40cb762ca02681
stefLesage <contact@webgiciel.com>
2025-12-22 23:24:07 +0100

    Ajout du fichier LICENCE

--- src/InitBureauSecuriteBundle.php
+++ src/InitBureauSecuriteBundle.php
@@ -3,7 +3,12 @@
 namespace Webgiciel2\InitBureauSecurite;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Webgiciel2\InitBureauSecurite\DependencyInjection\InitBureauSecuriteExtension;
 
 class InitBureauSecuriteBundle extends Bundle
 {
+    public function getContainerExtension(): ?InitBureauSecuriteExtension
+    {
+        return new InitBureauSecuriteExtension();
+    }
 }
