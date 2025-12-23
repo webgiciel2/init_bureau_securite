0de5f2b648f94229ab58c0b13f42a122e3c9528f
stefLesage <contact@webgiciel.com>
2025-12-23 14:13:20 +0100

    Correction code 1

--- src/Controller/ActivationController.php
+++ src/Controller/ActivationController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-use Symfony\Component\HttpFoundation\Response;
 
 class ActivationController extends AbstractController
 {
