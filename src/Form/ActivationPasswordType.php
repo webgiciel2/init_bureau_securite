3d5071eb20b89642cb2b076c990e496b5c70095e
stefLesage <contact@webgiciel.com>
2025-12-23 15:19:03 +0100

    Correction code 2

--- src/Form/ActivationPasswordType.php
+++ src/Form/ActivationPasswordType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\EqualTo;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ActivationPasswordType extends AbstractType
